5b132de9d94c7f5a894832bbfbf06130d4bf40eb
enderta <endertanver@gmail.com>
2022-09-05 21:52:16 +0100

    meals

--- src/Components/meals/Meals.jsx
+++ src/Components/meals/Meals.jsx
@@ -4,6 +4,7 @@ import './meal.css'
 
 const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
 const Meals = () => {
 const [meals, setMeals] = useState([])
 const [loading, setLoading] = useState(false)
@@ -35,14 +36,11 @@ const handleChange = (e) => {
     setSearchTerm(e.target.value)
 }
 if (loading) {
-    return <h2 className='section-title'>Loading...</h2>
+return <h2 className='section'>Loading...</h2>
 }
-if (meals.length < 1) {
-    return <section className="section" >
-      <h4>No meals matched your search term. Please try again.</h4>
-    </section>
-  }
-  const handelRandomMeal = () => {
+
+
+const handelRandomMeal = () => {
     setLoading(true)
     fetch(randomMealUrl)
         .then((res) => res.json())
@@ -55,42 +53,38 @@ if (meals.length < 1) {
 
 console.log(meals)
 
-  return (
+return (
     <>
     <h2 >Meals</h2>
     <form onSubmit={handleSubmit} className="search-form">
         <input type="text" placeholder="Search" className="form-input" value={searchTerm} onChange={handleChange} />
         <button type="submit" className="submit-btn">Search</button>
         <button onClick={handelRandomMeal} className="submit-btn">Random Meal</button>
-
     </form>
-    
     <section className="section">
-        
         <div className="section">
-            {meals.map((meal) => {
-                const { idMeal, strMeal, strMealThumb, strCategory, strArea } = meal
-                return (
-                    <article key={idMeal} className="meal">
-                        <img src={strMealThumb} alt={strMeal} />
-                        <div >
-                            <h4>{strMeal}</h4>
-                            <p>{strCategory}</p>
-                            <p>{strArea}</p>
-                            <a href={meal.strYoutube}>YouTube Recepie</a>
-                            
-                           
-
-                        </div>
-                    </article>
-                )
-            })}
+            {
+                meals.map((meal) => {
+                    const { idMeal, strMeal, strMealThumb,strArea,strCategory,strYoutube } = meal
+                    return(
+                        <article key={idMeal} className="meal">
+                                <img src={strMealThumb} alt={strMeal} />
+                                <div >
+                                    <h4>{strMeal}</h4>
+                                    <p>{strCategory}</p>
+                                    <p>{strArea}</p>
+                                    <a href={strYoutube}>YouTube Recepie</a>
+                                </div>
+                            </article>
+                    )
+                }
+                        ) 
+                }
+            
         </div>
     </section>
     </>
     )
-    
-
 }
 
 export default Meals
