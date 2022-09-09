3dfbf8341bee2724d0040893e9d2fa602695846b
enderta <endertanver@gmail.com>
2022-09-09 16:20:47 +0100

    dash

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Company from './Components/company/Company'
 import SearchEmps from './Components/company/SearchEmps'
+import MainApp from './Components/dash/MainApp'
 
 const App = () => {
   return (
     <div>
    
-   <Company />  
+   <MainApp />
     </div>
   )
 }
---
+++ src/Components/dash/MainApp.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './mainApp.css'
+import Tables from './Tables'
+import Lists from './Lists'
+import Create from './Create'
+import Graphs from './Graphs'
+
+export const MainApp = () => {
+  return (
+    <div>
+        <div className="parent">
+  <div className="div1">
+    <Tables />
+     </div>
+  <div className="div2"> 
+  <Lists />
+  </div>
+  <div className="div3"> 
+  <Graphs />
+  </div>
+  <div className="div4">
+    <Create />
+     </div>
+</div>
+
+    </div>
+  )
+}
+export default MainApp
