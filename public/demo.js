89a8eb52d8c7d8b3dcdcfca1b052a5815eb2372b
enderta <endertanver@gmail.com>
2022-09-17 21:42:36 +0100

    demo

--- public/demo.js
+++ public/demo.js
@@ -1,8 +1,31 @@
-let arr=[12,34,56,78,90];
+//function to get the value of the input field
+function wordToNumber(word) {
+    let num = 0;
+    switch (word) {
+        case "one":
+            num = 1;
+            break;
+        case "two":
+            num = 2;
+            break;
+        case "three":
+            num = 3;
+            break;
+        case "four":
+            num = 4;
+            break;
+        case "five":
+            num = 5;
+            break;
 
-// 1. forEach
+    }
+    return num;
+}
 
-arr.forEach((item,index)=>{
-    console.log(item,index);
-});
+console.log(wordToNumber('one'));
+function getMultiply(word, number) {
+    let num = wordToNumber(word);
+    return num * number;
+}
 
+console.log(getMultiply('one', 2));
