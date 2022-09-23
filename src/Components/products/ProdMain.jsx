31e57b9f61c6777e74362984db3e8d29ce9d5ed9
enderta <endertanver@gmail.com>
2022-09-23 15:49:55 +0100

    prod

--- src/Components/products/ProdMain.jsx
+++ src/Components/products/ProdMain.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import NavProducts from "./NavProducts";
 import Hero from "./Hero";
 import Pics from "./Pics";
+import ProdFooter from "./ProdFooter";
 
 const ProdMain = () => {
     return (
         <div>
 
 
-                <NavProducts/>
+            <NavProducts/>
 
 
-
-                <Hero/>
-                <Pics />
-
+            <Hero/>
+            <Pics/>
+            <ProdFooter/>
 
 
         </div>
