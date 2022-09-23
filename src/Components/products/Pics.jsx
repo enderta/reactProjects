066a96d4564cfd177f4af75ddb51c759d3273061
enderta <endertanver@gmail.com>
2022-09-23 15:47:27 +0100

    prod

--- src/Components/products/Pics.jsx
+++ src/Components/products/Pics.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
-
+import './pics.css';
 const Pics = () => {
+    const [prods, setProds] = React.useState([]);
+    React.useEffect(() => {
+        fetch('https://dummyjson.com/products')
+            .then(res => res.json())
+            .then(json => setProds(json.products))
+    }, [])
+
+
     return (
         <div>
-            <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-                <div className="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-                    <div className="my-3 py-3">
-                        <h2 className="display-5">Another headline</h2>
-                        <p className="lead">And an even wittier subheading.</p>
+            <div className="album py-5 bg-light">
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                        {
+                            prods.map(prod => (
+                                <div className="col">
+                                    <div className="card shadow-sm">
+                                        <img src={prod.thumbnail} alt="product" style={{ "height": "300px", "width": "300px" }} />
+                                        <div className="card-body">
+                                            <p className="card-text">{prod.title}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+
+                        }
                     </div>
-                    <div
-                        className="bg-light shadow-sm mx-auto"
-                        style={{ width: "80%", height: 300, borderRadius: "21px 21px 0 0" }}
-                    />
-                </div>
-                <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-                    <div className="my-3 p-3">
-                        <h2 className="display-5">Another headline</h2>
-                        <p className="lead">And an even wittier subheading.</p>
-                    </div>
-                    <div
-                        className="bg-dark shadow-sm mx-auto"
-                        style={{ width: "80%", height: 300, borderRadius: "21px 21px 0 0" }}
-                    />
                 </div>
             </div>
 
