2f56aa1fec3e9d887b9c06060eef3548fa855d71
enderta <endertanver@gmail.com>
2022-09-24 20:59:39 +0100

    mason

--- src/App.js
+++ src/App.js
@@ -3,11 +3,347 @@ import Navbar from "./Components/carosel/Navbar";
 import MainCarosel from "./Components/carosel/MainCarosel";
 import ProdMain from "./Components/products/ProdMain";
 import Mason from "./Components/masonry/Mason";
+import {Plock} from 'react-plock';
+
 
 const App = () => {
     return (
         <div>
-        <Mason />
+            <Plock>
+                <div
+                    style={{
+                        display: "grid",
+                        columnGap: 10,
+                        alignItems: "start",
+                        gridTemplateColumns: "repeat(4, 1fr)"
+                    }}
+                >
+                    <div
+                        data-testid="masonry-column"
+                        style={{ display: "grid", gridTemplateColumns: "100%", rowGap: 10 }}
+                    >
+                        <div height={409} className="App__Tiled-sc-1b1ajz2-0 gfZsHu">
+                            0
+                        </div>
+                        <div height={317} className="App__Tiled-sc-1b1ajz2-0 iyUOLM">
+                            4
+                        </div>
+                        <div height={405} className="App__Tiled-sc-1b1ajz2-0 iuLN">
+                            8
+                        </div>
+                        <div height={322} className="App__Tiled-sc-1b1ajz2-0 zgVkq">
+                            12
+                        </div>
+                        <div height={407} className="App__Tiled-sc-1b1ajz2-0 jTZSkE">
+                            16
+                        </div>
+                        <div height={446} className="App__Tiled-sc-1b1ajz2-0 UrSKT">
+                            20
+                        </div>
+                        <div height={406} className="App__Tiled-sc-1b1ajz2-0 bWVWoF">
+                            24
+                        </div>
+                        <div height={442} className="App__Tiled-sc-1b1ajz2-0 hZtnHG">
+                            28
+                        </div>
+                        <div height={441} className="App__Tiled-sc-1b1ajz2-0 RGuHz">
+                            32
+                        </div>
+                        <div height={338} className="App__Tiled-sc-1b1ajz2-0 hAMKNF">
+                            36
+                        </div>
+                        <div height={378} className="App__Tiled-sc-1b1ajz2-0 WXDaK">
+                            40
+                        </div>
+                        <div height={357} className="App__Tiled-sc-1b1ajz2-0 eIZrrq">
+                            44
+                        </div>
+                        <div height={419} className="App__Tiled-sc-1b1ajz2-0 hkFmOh">
+                            48
+                        </div>
+                        <div height={409} className="App__Tiled-sc-1b1ajz2-0 bPxnqL">
+                            52
+                        </div>
+                        <div height={427} className="App__Tiled-sc-1b1ajz2-0 hYehty">
+                            56
+                        </div>
+                        <div height={394} className="App__Tiled-sc-1b1ajz2-0 bcQpKw">
+                            60
+                        </div>
+                        <div height={441} className="App__Tiled-sc-1b1ajz2-0 hfQAcQ">
+                            64
+                        </div>
+                        <div height={377} className="App__Tiled-sc-1b1ajz2-0 bjqnwR">
+                            68
+                        </div>
+                        <div height={393} className="App__Tiled-sc-1b1ajz2-0 jcVMQK">
+                            72
+                        </div>
+                        <div height={340} className="App__Tiled-sc-1b1ajz2-0 kMIsQm">
+                            76
+                        </div>
+                        <div height={372} className="App__Tiled-sc-1b1ajz2-0 cVVnLD">
+                            80
+                        </div>
+                        <div height={412} className="App__Tiled-sc-1b1ajz2-0 vyyIQ">
+                            84
+                        </div>
+                        <div height={374} className="App__Tiled-sc-1b1ajz2-0 bCnAoF">
+                            88
+                        </div>
+                        <div height={312} className="App__Tiled-sc-1b1ajz2-0 eUXhYt">
+                            92
+                        </div>
+                        <div height={415} className="App__Tiled-sc-1b1ajz2-0 fnqJMe">
+                            96
+                        </div>
+                        <div height={446} className="App__Tiled-sc-1b1ajz2-0 kjBHLm">
+                            100
+                        </div>
+                    </div>
+                    <div
+                        data-testid="masonry-column"
+                        style={{ display: "grid", gridTemplateColumns: "100%", rowGap: 10 }}
+                    >
+                        <div height={448} className="App__Tiled-sc-1b1ajz2-0 uULVs">
+                            1
+                        </div>
+                        <div height={314} className="App__Tiled-sc-1b1ajz2-0 bUGvVN">
+                            5
+                        </div>
+                        <div height={359} className="App__Tiled-sc-1b1ajz2-0 ftgVUR">
+                            9
+                        </div>
+                        <div height={431} className="App__Tiled-sc-1b1ajz2-0 fKAjJu">
+                            13
+                        </div>
+                        <div height={339} className="App__Tiled-sc-1b1ajz2-0 jQtVxD">
+                            17
+                        </div>
+                        <div height={430} className="App__Tiled-sc-1b1ajz2-0 fklcGO">
+                            21
+                        </div>
+                        <div height={331} className="App__Tiled-sc-1b1ajz2-0 cCsYlv">
+                            25
+                        </div>
+                        <div height={330} className="App__Tiled-sc-1b1ajz2-0 ispCwx">
+                            29
+                        </div>
+                        <div height={319} className="App__Tiled-sc-1b1ajz2-0 hFNaIM">
+                            33
+                        </div>
+                        <div height={440} className="App__Tiled-sc-1b1ajz2-0 hfPPGA">
+                            37
+                        </div>
+                        <div height={411} className="App__Tiled-sc-1b1ajz2-0 eNxUnW">
+                            41
+                        </div>
+                        <div height={413} className="App__Tiled-sc-1b1ajz2-0 dnJLRD">
+                            45
+                        </div>
+                        <div height={405} className="App__Tiled-sc-1b1ajz2-0 jJptdO">
+                            49
+                        </div>
+                        <div height={336} className="App__Tiled-sc-1b1ajz2-0 fngYsU">
+                            53
+                        </div>
+                        <div height={424} className="App__Tiled-sc-1b1ajz2-0 ddlnnH">
+                            57
+                        </div>
+                        <div height={426} className="App__Tiled-sc-1b1ajz2-0 jABEpp">
+                            61
+                        </div>
+                        <div height={322} className="App__Tiled-sc-1b1ajz2-0 cZgeWP">
+                            65
+                        </div>
+                        <div height={322} className="App__Tiled-sc-1b1ajz2-0 zgVkq">
+                            69
+                        </div>
+                        <div height={329} className="App__Tiled-sc-1b1ajz2-0 hHtLDl">
+                            73
+                        </div>
+                        <div height={422} className="App__Tiled-sc-1b1ajz2-0 hTqPmI">
+                            77
+                        </div>
+                        <div height={377} className="App__Tiled-sc-1b1ajz2-0 jzUESF">
+                            81
+                        </div>
+                        <div height={428} className="App__Tiled-sc-1b1ajz2-0 bwcwMm">
+                            85
+                        </div>
+                        <div height={426} className="App__Tiled-sc-1b1ajz2-0 dPTedG">
+                            89
+                        </div>
+                        <div height={404} className="App__Tiled-sc-1b1ajz2-0 TUuQE">
+                            93
+                        </div>
+                        <div height={340} className="App__Tiled-sc-1b1ajz2-0 gRuoBm">
+                            97
+                        </div>
+                    </div>
+                    <div
+                        data-testid="masonry-column"
+                        style={{ display: "grid", gridTemplateColumns: "100%", rowGap: 10 }}
+                    >
+                        <div height={322} className="App__Tiled-sc-1b1ajz2-0 cQcJFF">
+                            2
+                        </div>
+                        <div height={448} className="App__Tiled-sc-1b1ajz2-0 fltsyI">
+                            6
+                        </div>
+                        <div height={394} className="App__Tiled-sc-1b1ajz2-0 kFCngr">
+                            10
+                        </div>
+                        <div height={438} className="App__Tiled-sc-1b1ajz2-0 kxtdoe">
+                            14
+                        </div>
+                        <div height={418} className="App__Tiled-sc-1b1ajz2-0 jwHHIk">
+                            18
+                        </div>
+                        <div height={340} className="App__Tiled-sc-1b1ajz2-0 eUxnjN">
+                            22
+                        </div>
+                        <div height={338} className="App__Tiled-sc-1b1ajz2-0 lgLuML">
+                            26
+                        </div>
+                        <div height={333} className="App__Tiled-sc-1b1ajz2-0 AEczN">
+                            30
+                        </div>
+                        <div height={329} className="App__Tiled-sc-1b1ajz2-0 jRvOjG">
+                            34
+                        </div>
+                        <div height={399} className="App__Tiled-sc-1b1ajz2-0 chnLxu">
+                            38
+                        </div>
+                        <div height={442} className="App__Tiled-sc-1b1ajz2-0 bnHiwW">
+                            42
+                        </div>
+                        <div height={350} className="App__Tiled-sc-1b1ajz2-0 iREZBX">
+                            46
+                        </div>
+                        <div height={436} className="App__Tiled-sc-1b1ajz2-0 hYieEL">
+                            50
+                        </div>
+                        <div height={400} className="App__Tiled-sc-1b1ajz2-0 eSZUrn">
+                            54
+                        </div>
+                        <div height={300} className="App__Tiled-sc-1b1ajz2-0 BqqDx">
+                            58
+                        </div>
+                        <div height={362} className="App__Tiled-sc-1b1ajz2-0 iMmrDy">
+                            62
+                        </div>
+                        <div height={340} className="App__Tiled-sc-1b1ajz2-0 jjHkoy">
+                            66
+                        </div>
+                        <div height={349} className="App__Tiled-sc-1b1ajz2-0 fgHxNr">
+                            70
+                        </div>
+                        <div height={408} className="App__Tiled-sc-1b1ajz2-0 hYJJWl">
+                            74
+                        </div>
+                        <div height={398} className="App__Tiled-sc-1b1ajz2-0 kHDovX">
+                            78
+                        </div>
+                        <div height={384} className="App__Tiled-sc-1b1ajz2-0 fwVMHv">
+                            82
+                        </div>
+                        <div height={450} className="App__Tiled-sc-1b1ajz2-0 jylcaK">
+                            86
+                        </div>
+                        <div height={311} className="App__Tiled-sc-1b1ajz2-0 lgIomV">
+                            90
+                        </div>
+                        <div height={438} className="App__Tiled-sc-1b1ajz2-0 fIcKuO">
+                            94
+                        </div>
+                        <div height={376} className="App__Tiled-sc-1b1ajz2-0 jSvDPk">
+                            98
+                        </div>
+                    </div>
+                    <div
+                        data-testid="masonry-column"
+                        style={{ display: "grid", gridTemplateColumns: "100%", rowGap: 10 }}
+                    >
+                        <div height={342} className="App__Tiled-sc-1b1ajz2-0 cfsBHl">
+                            3
+                        </div>
+                        <div height={365} className="App__Tiled-sc-1b1ajz2-0 kqTWut">
+                            7
+                        </div>
+                        <div height={362} className="App__Tiled-sc-1b1ajz2-0 jZJatf">
+                            11
+                        </div>
+                        <div height={413} className="App__Tiled-sc-1b1ajz2-0 dylpcT">
+                            15
+                        </div>
+                        <div height={303} className="App__Tiled-sc-1b1ajz2-0 iDNgvc">
+                            19
+                        </div>
+                        <div height={329} className="App__Tiled-sc-1b1ajz2-0 fnkqdl">
+                            23
+                        </div>
+                        <div height={328} className="App__Tiled-sc-1b1ajz2-0 ekeiSZ">
+                            27
+                        </div>
+                        <div height={386} className="App__Tiled-sc-1b1ajz2-0 fzxFfD">
+                            31
+                        </div>
+                        <div height={383} className="App__Tiled-sc-1b1ajz2-0 lpbsGJ">
+                            35
+                        </div>
+                        <div height={450} className="App__Tiled-sc-1b1ajz2-0 hhMdXs">
+                            39
+                        </div>
+                        <div height={418} className="App__Tiled-sc-1b1ajz2-0 jwHHIk">
+                            43
+                        </div>
+                        <div height={419} className="App__Tiled-sc-1b1ajz2-0 hJnogh">
+                            47
+                        </div>
+                        <div height={436} className="App__Tiled-sc-1b1ajz2-0 hICRzY">
+                            51
+                        </div>
+                        <div height={424} className="App__Tiled-sc-1b1ajz2-0 esRsJw">
+                            55
+                        </div>
+                        <div height={393} className="App__Tiled-sc-1b1ajz2-0 jOvKTv">
+                            59
+                        </div>
+                        <div height={366} className="App__Tiled-sc-1b1ajz2-0 hNNLsu">
+                            63
+                        </div>
+                        <div height={301} className="App__Tiled-sc-1b1ajz2-0 jOLnos">
+                            67
+                        </div>
+                        <div height={409} className="App__Tiled-sc-1b1ajz2-0 kKcjiL">
+                            71
+                        </div>
+                        <div height={339} className="App__Tiled-sc-1b1ajz2-0 taLsM">
+                            75
+                        </div>
+                        <div height={378} className="App__Tiled-sc-1b1ajz2-0 WXDaK">
+                            79
+                        </div>
+                        <div height={412} className="App__Tiled-sc-1b1ajz2-0 iUoeBJ">
+                            83
+                        </div>
+                        <div height={412} className="App__Tiled-sc-1b1ajz2-0 hbIkwy">
+                            87
+                        </div>
+                        <div height={424} className="App__Tiled-sc-1b1ajz2-0 kIyJgn">
+                            91
+                        </div>
+                        <div height={391} className="App__Tiled-sc-1b1ajz2-0 jKyIxR">
+                            95
+                        </div>
+                        <div height={324} className="App__Tiled-sc-1b1ajz2-0 codfQH">
+                            99
+                        </div>
+                    </div>
+                </div>
+
+            </Plock>
         </div>
     )
 }
--- src/Components/demo.js
+++ src/Components/demo.js
@@ -1,107 +1,29 @@
-const books = [
-    {
-        title: "Masumiyet Muzesi",
-        Autor: "Orhan Pamuk",
-        alreadyRead:true
-    },
+    /*
+    * very budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
 
-    {
-        title: "An Idiot Abroad",
-        Autor: "Karl Pilkington",
-        alreadyRead:true
-    },
-    {
-        title: "Simyaci",
-        Autor: "Paulo Coelho ",
-        alreadyRead:true
-    },
-    {
-        title: " Lord of the Rings",
-        Autor: "J. R. R. Tolkien",
-        alreadyRead:false
-    },
-    {
-        title: "Queen of Our Times: The Life of Elizabeth II",
-        Autor: "Robert Hardman",
-        alreadyRead:false
-    },
-];
-let logBooks1 = () => {
-    books.map((book) => {
-        console.log(book.title + " by " + book.Autor);
+    Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+    Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+
+    If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+    Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+    var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+    var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+    aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+    aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+    * */
+    let aliasGen = (firstName, lastName) => {
+        let first = firstName[0].toUpperCase();
+        let last = lastName[0].toUpperCase();
+       let firstObj = {A: 'Alpha', B: 'Beta', C: 'Cache', D: 'Data', E: 'Energy', F: 'Function', G: 'Glitch', H: 'Half-life', I: 'Ice', J: 'Java', K: 'Keystroke', L: 'Logic', M: 'Malware', N: 'Nagware', O: 'OS', P: 'Phishing', Q: 'Quantum', R: 'RAD', S: 'Strike', T: 'Trojan', U: 'Ultraviolet', V: 'Vanilla', W: 'WiFi', X: 'Xerox', Y: 'Y', Z: 'Zero'};
+       let lastObj = {A: 'Analogue', B: 'Bomb', C: 'Catalyst', D: 'Discharge', E: 'Electron', F: 'Faraday', G: 'Gig', H: 'Hacker', I: 'IP', J: 'Jabber', K: 'Killer', L: 'Lazer', M: 'Mike', N: 'Nuke', O: 'Overclock', P: 'Payload', Q: 'Quark', R: 'Roy', S: 'Spy', T: 'T-Rex', U: 'Unit', V: 'Virus', W: 'Worm', X: 'X', Y: 'Yob', Z: 'Zombie'};
+         if(firstObj[first] && lastObj[last]){
+                return `${firstObj[first]} ${lastObj[last]}`;
+         }
+            return "Your name must start with a letter from A - Z.";
+
     }
-    )
-}
-let logBooks2 =()=>{
-    books.map((book)=>{
-        if(book.alreadyRead){
-            console.log("You already read "+ book.title + " by " + book.Autor);
-        }else{
-            console.log("You still need to read "+ book.title + " by " + book.Autor);
-        }
-    })
-};
-
-
-/*
-=====
-Exercise 2
-=====
-Now modify the function, using an if/else statement to change the output depending on whether you have read it yet or not.
-If you've read it, log a string like 'You've already read "The Hobbit" by J.R.R. Tolkien',
-and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
-You will need to modify the tests to check the correct output. If you have already learnt about red-green refactoring,
-remember to practice:
-- first change the test to the value that should be output,
-- run the test to check that your test goes red
-- now change your code to make the test pass
-As an example for this exercise, you might do the following steps
-- Modify the tests so that they all say 'You've already read <name> by <author>'
-- Run the test (they will all fail)
-- Modify your code so that it logs 'You've already read <name> by <author>'
-- Run the test (they will all pass again)
-- Modify your code making all the books alreadyRead:false and adding the if/else so that it logs 'You still need to read <name> by <author>'
-- Run the test (they will all fail)
-- Modify the tests to contain the correct list of statements (whether you've read the book or not)
-- Run the test (the test will fail but there will be some successful results)
-- Modify the books so that they have the correct alreadyRead value
-- All tests should turn green!!
-**/
-
-// Write your code here
-
-
-/* ======= TESTS - DO MODIFY (!!!) =====
-- To run the tests for this exercise, run `npm test -- --testPathPattern 8-reading-list.js`
-- To run all exercises/tests in the mandatory folder, run `npm test`
-- (Reminder: You must have run `npm install` one time before this will work!)
-*/
-
-test("books are logged", function() {
-    expectLogBooksToLog([
-        "Masumiyet Muzesi by Orhan Pamuk",
-        "An Idiot Abroad by Karl Pilkington",
-        "Simyaci by Paulo Coelho ",
-        " Lord of the Rings by J. R. R. Tolkien",
-        "Queen of Our Times: The Life of Elizabeth II by Robert Hardman",
-
-    ]);
-});
-
-/* ======= TESTS - DO NOT MODIFY ===== */
-/*
-* Assert that when the function logBooks is called, the values in the expectedValues array are logged in order via console.log.
-*
-* - If the number of calls to console.log does not match the number of elements in the array, the test will fail
-* - If the calls to console.log do not contain the strings in the expectedValue array, the test will fail
-*
-* You do not need to understand how this function works to successfully complete the exercise.
-*/
-function expectLogBooksToLog(expectedValues) {
-    const consoleLogSpy = jest.spyOn(console, 'log');
-    logBooks();
-    expect(consoleLogSpy).toBeCalledTimes(expectedValues.length);
-    expectedValues.forEach((value, i) => {
-        expect(consoleLogSpy).nthCalledWith(i+1, value);
-    });}
-consoleLogSpy.mockRestore()
+    console.log(aliasGen('123abc', 'Petrovic'));
