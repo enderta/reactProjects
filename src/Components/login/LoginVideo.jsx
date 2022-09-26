8c0a04aac605061b30af34c8ca3111f243442ee8
enderta <endertanver@gmail.com>
2022-09-26 13:52:43 +0100

    login

--- src/Components/login/LoginVideo.jsx
+++ src/Components/login/LoginVideo.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './loginvideo.css'
+
 const LoginVideo = () => {
     const [user, setUser] = React.useState(null);
     const [password, setPassword] = React.useState(null);
 
 
     return (
-
         <div>
-
             <video autoPlay="autoplay" loop='loop' muted>
                 <source
                     src={require('../Clouds - 64759.mp4')}
@@ -16,23 +15,20 @@ const LoginVideo = () => {
                 />
                 Your browser does not support the video tag.
             </video>
-            <form >
+            <form>
                 <h1>Login</h1>
                 <input
                     type="text"
                     placeholder="Username"
-
                     onChange={(e) => setUser(e.target.value)}
                 />
                 <input
                     type="password"
                     placeholder="Password"
-
                     onChange={(e) => setPassword(e.target.value)}
                 />
                 <button
                     type="submit"
-
                     onClick={(e) => {
                         e.preventDefault();
                         console.log(user, password);
