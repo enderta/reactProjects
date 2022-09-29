117aadea85c5cac9a68154768ec8c2238f6cf2fb
enderta <endertanver@gmail.com>
2022-09-29 10:53:39 +0100

    login

--- src/Components/album/Tables.jsx
+++ src/Components/album/Tables.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import {Button} from "react-bootstrap";
 const Tables = (props) => {
 const [products, setProducts] = React.useState([]);
 React.useEffect(() => {
@@ -18,37 +14,42 @@ setView(!viewbtn);
 
 }
     return (
+        <>
+            <div className="album py-5 bg-light">
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" >
+                        {
+                            products.map(product => (
+                                <div className="col">
+                                    <div className="card shadow-sm" >
+                                        <img src={product.thumbnail} alt="product" style={{"height":"300px","width":"300px"}}  />
+                                        <div className="card-body">
+                                            <p className="card-text">{product.title}</p>
 
-        <div className="album py-5 bg-light">
-            <div className="container">
-                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" >
-                    {
-                        products.map(product => (
-                            <div className="col">
-                                <div className="card shadow-sm" >
-                                    <img src={product.thumbnail} alt="product" style={{"height":"300px","width":"300px"}}  />
-                                    <div className="card-body">
-                                        <p className="card-text">{product.title}</p>
+                                            <div className="d-flex justify-content-between align-items-center">
+                                                <div className="btn-group">
+                                                    <button type="button" className="btn btn-sm btn-outline-secondary" onClick={handleView}>View</button>
 
-                                        <div className="d-flex justify-content-between align-items-center">
-                                            <div className="btn-group">
-                                                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                                </div>
+
                                             </div>
-                                            <small className="text-muted">{product.price}$</small>
+                                            {viewbtn && <p>{product.description}</p>}
+                                            {viewbtn && <p>{product.price}</p>}
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
+                            ))
 
 
 
-                    }
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        </>
+    )
+
+
 };
 
 export default Tables;
